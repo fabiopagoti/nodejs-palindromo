6fe74fb10e8cfd5d9457c1aa35a44379608dba40
Fabio Pagoti <fabio@duxinnovation.com>
2017-08-30 21:51:08 -0300

    verifica todos caracteres

--- palinnilap/index.js
+++ palinnilap/index.js
@@ -3,17 +3,16 @@
 let assert = require('assert')
 
 const checkPalinDromo = function(word){
-
-	for (var i = 0; i < word.length; i++) {
+	for (var i = 0; i < Math.ceil(word.length / 2); i++) {
 
 		try{
-			assert.equal(word.charAt(0), word.charAt(word.length - 1))
+			console.log(`${word.charAt(i)} - ${word.charAt(word.length - 1 - i)}` )
+			assert.equal(word.charAt(i), word.charAt(word.length - 1 - i))
 		}
 		catch(err) {
 			return false
 		}
 	}
-	
 	return true
 }
 
